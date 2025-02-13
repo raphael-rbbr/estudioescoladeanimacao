162deacbd2a52ba25dbec2e6da177cb8822d4bb3
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2025-02-13 17:30:19 +0100

    config for db on amazon

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 estudioescoladeanimacao.wsgi
+python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 estudioescoladeanimacao.wsgi
