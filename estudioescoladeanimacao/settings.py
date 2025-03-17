d5e404b62925d8db638de7f7ace2be3ab519a0c3
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2025-03-17 19:53:19 +0100

    allowed hosts changed

--- estudioescoladeanimacao/settings.py
+++ estudioescoladeanimacao/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zwt6_ju#u_fmf(46^4tntmd8c%b75nca5d7lt646y!t=*5*lvh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
